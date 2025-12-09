e414060530cd40c2e6fe15c53a265094b8695a6b
Jose Salas <chiquidaw@gmail.com>
2025-12-09 18:28:12 +0000

    Finalizado con guardado local.

--- script.js
+++ script.js
@@ -1,24 +1,36 @@
 const countryList = document.getElementById('country-list');
 const valuesList = document.getElementById('values-list');
-const lenguagesList = document.getElementById('lenguages-list');
+const languagesList = document.getElementById('lenguages-list');
 
 fetch('https://restcountries.com/v3.1/all?fields=name')
     .then(response => response.json())
     .then(countries => {
+        countries.sort((a, b) => a.name.common.localeCompare(b.name.common));
         countries.forEach(country => insertCountry(country));
+
+        const savedCountry = localStorage.getItem('selectedCountry');
+        
+        if (savedCountry) {
+            countryList.value = savedCountry;
+            countryList.dispatchEvent(new Event('change'));
+        }
     });
 
 function insertCountry(country) {
     const option = document.createElement('option');
     option.textContent = `${country.name.common}`;
+    option.value = `${country.name.common}`;
     countryList.append(option);
 }
 
 countryList.addEventListener('change', (event) => {
+    localStorage.setItem('selectedCountry', countryList.value);
+
     fetch(`https://restcountries.com/v3.1/name/${countryList.value}`)
         .then(response => response.json())
         .then(values => {
             valuesList.textContent = "";
+            languagesList.textContent = "";
             values.forEach(value => insertValue(value));
         });
 })
@@ -29,15 +41,44 @@ function insertValue(value) {
     const flag = document.createElement('img');
     const languages = document.createElement('li');
 
-    capital.textContent = "Capital: " + `${value.capital}`;
-    let key = Object.keys(value.currencies)[0];
-    currency.textContent = "Moneda: " + `${value.currencies[key].name}`;
-    flag.src = `${value.flags.svg}`;
-    key = Object.keys(value.languages);
-    languages.textContent = `Lengua (Una de ellas): ${value.languages[key[0]]}`;
+    capital.textContent = "Capital: " + (value.capital ? value.capital[0] : "N/A");
+    
+    if(value.currencies) {
+        let key = Object.keys(value.currencies)[0];
+        currency.textContent = "Currency: " + `${value.currencies[key].name}`;
+    } else {
+        currency.textContent = "Currency: N/A";
+    }
 
+    flag.src = `${value.flags.svg}`;
+    
     valuesList.append(capital);
     valuesList.append(currency);
-    valuesList.append(languages);
+
+    if(value.languages) {
+        let key = Object.keys(value.languages);
+        languages.textContent = `${value.languages[key[0]]}`;
+        languages.dataset.id = "language";
+        valuesList.append(languages);
+    }
+
     valuesList.append(flag);
+}
+
+valuesList.addEventListener('click', (event) => {
+    const lang = event.target;
+    if (lang.dataset.id === 'language') {
+        fetch(`https://restcountries.com/v3.1/lang/${lang.textContent}`)
+            .then(response => response.json())
+            .then(languages => {
+                languagesList.textContent = '';
+                languages.forEach(language => insertLanguage(language));
+            });
+    }
+});
+
+function insertLanguage(language) {
+    const img = document.createElement('img');
+    img.src = `${language.flags.svg}`;
+    languagesList.append(img);
 }
